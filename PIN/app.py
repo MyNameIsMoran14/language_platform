2dfcdf9d79f58b1a6c7079139e6d78a64ddd1bfb
MyNameIsMoran14 <margotaraeva@gmail.com>
2025-06-17 16:41:24 +0300

    menu routes

--- PIN/app.py
+++ PIN/app.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("pages/index.html")
+    return render_template("pages/index.html") 
+
+@app.route("/learning")
+def learning():
+    return render_template("pages/learning_page.html") 
+
+@app.route('/team')
+def inteam():
+    return render_template('pages/team.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('pages/contacts.html')
+
+@app.route('/test')
+def test():
+    return render_template('pages/test_chinese.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
